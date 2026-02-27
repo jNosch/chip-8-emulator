09e6ff36b3c07bb12f9040130e5d7ed1e837565c
jNosch <janoschaldemann@gmx.ch>
2026-02-27 16:35:30 +0100

    added the run and stop functions in the Emulator class, aswell as used the loadRom function

--- src/mainCycle/chip8Emulator.cpp
+++ src/mainCycle/chip8Emulator.cpp
@@ -2,6 +2,29 @@
 // Created by RageZ on 13/01/2026.
 //
 
+#include <stdexcept>
 #include "chip8Emulator.h"
 
+chip8Emulator::chip8Emulator() : running(false)
+{}
+void chip8Emulator::loadRom(const std::string &rom_path) {
+        if (!core.loadRom(rom_path)) {
+            throw std::runtime_error("failed to load rom" + rom_path);
+        }
+}
+void chip8Emulator::run() {
+    running = true;
 
+    while(running) {
+        core.cycle();
+
+
+        //add the visual initialization here
+    }
+}
+
+
+void chip8Emulator::stop() {
+
+    running = false;
+}
